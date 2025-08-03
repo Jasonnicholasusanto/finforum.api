5db7a4e93b54ce4362720504414546853db86e31
Jason <jasonnichola@student.unimelb.edu.au>
2025-08-04 00:31:22 +1000

    Initial setup of project repo

---
+++ app/models/stocks.py
@@ -0,0 +1,9 @@
+from typing import Literal
+from pydantic import BaseModel
+
+
+class TickerItem(BaseModel):
+    symbol: str
+    start_date: str
+    end_date: str
+    interval: Literal["1m", "2m", "5m", "15m", "30m", "60m", "90m", "1h", "1d", "5d", "1wk", "1mo", "3mo"] = "1m"
---
+++ app/routers/stocks.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter
+from app.models.stocks import TickerItem
+import yfinance as yf
+
+
+router = APIRouter()
+
+@router.post("/get-ticker-data/")
+async def get_ticker_data(tickerItem: TickerItem):
+    try:
+        ticker_data = yf.Ticker(tickerItem.symbol)
+        ticker_data_hist = ticker_data.history(
+            start=tickerItem.start_date,
+            end=tickerItem.end_date,
+            interval=tickerItem.interval
+        )
+        return ticker_data_hist.to_dict()
+    except Exception as e:
+        return {"error": str(e)}
+
