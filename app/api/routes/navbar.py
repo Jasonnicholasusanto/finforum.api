bf6dc57ebfa6df43df5fec2b001264198755b9c4
Jason <jasonnicholassusanto@gmail.com>
2025-10-15 01:12:59 +1100

    Secured API route

--- app/api/routes/navbar.py
+++ app/api/routes/navbar.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter
 from app.schemas.navbar_routes import NavbarRoutes
-from app.api.deps import SessionDep
+from app.api.deps import CurrentUser, SessionDep
 from app.services.navbar_routes_service import get_navbar_routes
 
 
 router = APIRouter(prefix="/navbar", tags=["navbar"])
 
 @router.get("/items", response_model=NavbarRoutes)
-async def get_navbar_items(db: SessionDep):
+async def get_navbar_items(db: SessionDep, user: CurrentUser):
     """
     Retrieve a list of navbar items.
     """
